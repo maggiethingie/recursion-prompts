99cb143eda42f82c1e2188f6d6f5a5cfe64fcb6a
Maggie Aytac <maggie.aytac@gmail.com>
2021-01-11 14:55:33 -0800

    19-21 completed

--- src/recursion.js
+++ src/recursion.js
@@ -268,17 +268,41 @@ var buildList = function(value, length) {
 // For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
 // fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
 var fizzBuzz = function(n) {
+  if (n === 0) {
+    return [];
+  }
+  if (n % 3 === 0 && n % 5 === 0) {
+    return [...fizzBuzz(n - 1), 'FizzBuzz'];
+  }
+  if (n % 3 === 0) {
+    return [...fizzBuzz(n - 1), 'Fizz'];
+  }
+  if (n % 5 === 0) {
+    return [...fizzBuzz(n - 1), 'Buzz'];
+  }
+  return [...fizzBuzz(n - 1), `${n}`];
 };
 
 // 20. Count the occurence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+  if (array.length === 0) {
+    return 0;
+  }
+  if (array[0] === value) {
+    return 1 + countOccurrence(array.slice(1), value);
+  }
+  return countOccurrence(array.slice(1), value);
 };
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
 var rMap = function(array, callback) {
+  if (array.length === 0) {
+    return [];
+  }
+  return [callback(array[0]), ...rMap(array.slice(1), callback)];
 };
 
 // 22. Write a function that counts the number of times a key occurs in an object.
@@ -286,6 +310,7 @@ var rMap = function(array, callback) {
 // countKeysInObj(obj, 'r') // 1
 // countKeysInObj(obj, 'e') // 2
 var countKeysInObj = function(obj, key) {
+
 };
 
 // 23. Write a function that counts the number of times a value occurs in an object.
